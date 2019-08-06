5bdd8eec46bdac5959e1ea7d7fcc668e91400de9
GiovaniCuenca <giovani.cuenca@gmail.com>
2019-08-06 00:14:34 -0300

    Update01

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+//Importar o Express - Require(Nome da Dependência)
 const express = require("express");
-
+//Iniciar a aplicação e receber o Express() **Express exporta função*
 const app = express();
 
 app.use(express.json());
@@ -11,17 +12,19 @@ const projects = [
     tasks: ["Nova tarefa"]
   }
 ];
-
+//Pegar informação de um ID,
+//Req: Requisição
+//Res: Retornar resposta
 app.get("/projects/:index", (req, res) => {
   const { index } = req.params;
 
   return res.json(projects[index]);
 });
-
+//Pegar informação de todo o Array projects
 app.get("/projects", (req, res) => {
   return res.json(projects);
 });
-
+//Adicionar novo ID e nome de projeto
 app.post("/projects", (req, res) => {
   const { id, title } = req.body;
 
@@ -35,5 +38,5 @@ app.post("/projects", (req, res) => {
 
   return res.json(projects);
 });
-
+//Desginar a porta para a variável APP declarada no inicio do código
 app.listen(3000);
